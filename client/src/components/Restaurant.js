c7134656495c2755f0316157c1778c5a2abe635a
Harneet <harneet.sandhu@developersinstitute.co.nz>
2023-08-21 13:25:10 -0700

    add user story 1 and 2 in client side

--- client/src/components/Restaurant.js
+++ client/src/components/Restaurant.js
@@ -10,8 +10,10 @@ const Restaurant = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const fetchUrl = `http://localhost:5001/restaurants/${id}`;
+      const fetchUrl =  await fetch(`http://localhost:5001/restaurants/${id}`);
       // FIXME: Make a fetch request and call setRestaurant with the response body
+      const data = await fetchUrl.json();
+      setRestaurant(data);
       setIsLoading(false);
     };
     fetchData();
@@ -24,6 +26,11 @@ const Restaurant = () => {
   return (
     <>
       <CreateReservation restaurantName={restaurant.name} />
+      <div key={restaurant.id}>
+      <img src={restaurant.image} alt={restaurant.name} />
+          <p> {restaurant.name} </p>
+          <p> {restaurant.description} </p>
+          </div>
     </>
   );
 };
--- client/src/components/RestaurantList.js
+++ client/src/components/RestaurantList.js
@@ -1,9 +1,34 @@
+import React, {useState, useEffect} from "react"
 import "./RestaurantList.css";
+import { Link } from "react-router-dom";
 
 const RestaurantList = () => {
+  const [restaurants, setRestaurants] = useState([])
+
+  const fetchData = async () => {
+    const response = await fetch ("http://localhost:5001/restaurants");
+    const data = await response.json();
+    setRestaurants(data);
+    };
+  
+    useEffect(() =>{
+      fetchData();
+    }, []);
+
   return (
     <>
       <h1>Restaurants</h1>
+      {restaurants.map((restaurant) => (
+        <div key={restaurant.id}>
+          <img src={restaurant.image} alt={restaurant.name} />
+          <p> {restaurant.name} </p>
+          <p> {restaurant.description} </p>
+          
+          <Link to={`/restaurants/${restaurant.id}`} >
+            Reserve now <code>&#8594;</code>
+          </Link>
+        </div>
+      ))}
     </>
   );
 };
