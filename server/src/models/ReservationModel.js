32f804ac0ade83c8d76b55ead396c200607dd620
Harneet <harneet.sandhu@developersinstitute.co.nz>
2023-08-24 21:36:30 -0700

    Add userId

--- server/src/models/ReservationModel.js
+++ server/src/models/ReservationModel.js
@@ -6,7 +6,7 @@ const reservationSchema = new Schema ({
     partySize: { type: Number, required: true},
     date:{ type: Date, required: true},
     restaurantName: { type: String, required: true},
-    createdBy: { type: String, required: true}
+    userId: { type: String, required: true}
 });
 
 const Reservation = mongoose.model("Reservation", reservationSchema);
